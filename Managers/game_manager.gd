d55532c22da845d690f0fa363370de090ec102e9
MelDidntListen <melodyisnotlistening@gmail.com>
2025-04-24 20:35:53 -0700

    Add new standin cursors
    Standin cursors that aren't as obnoxiously huge as the other ones. Fixed game manager for calendar so I can reference mouse position

--- Managers/game_manager.gd
+++ Managers/game_manager.gd
@@ -1,8 +1,9 @@
 extends Node2D
 class_name Gameplay
 
-@onready var hand_cursor_normal = preload("res://Assets/2d_assets/cursor_standin.png")
-@onready var hand_cursor_active = preload("res://Assets/2d_assets/cursor_active.svg")
+@onready var hand_cursor_normal = preload("res://Assets/2d_assets/cursors/cursor_normal.png")
+@onready var hand_cursor_active = preload("res://Assets/2d_assets/cursors/cursor_active.png")
+@onready var hand_cursor_off = preload("res://Assets/2d_assets/cursors/cursor_off.png")
 
 static var current_activity:Activity
 static var current_age = {
@@ -21,11 +22,14 @@ func _ready() -> void:
 func player_click():
 	pass
 
-func change_cursor_hand():
+func change_cursor_active():
 	Input.set_custom_mouse_cursor(hand_cursor_active)
 
 func change_cursor_back():
 	Input.set_custom_mouse_cursor(hand_cursor_normal)
+
+func change_cursor_off():
+	Input.set_custom_mouse_cursor(hand_cursor_off)
 
 func setAge(newAge):
 	current_age = newAge
@@ -36,3 +40,7 @@ func getAge():
 	print("Getting age called")
 	print("Current age is ", current_age, ". Is this redundant?")
 	return current_age
+
+
+func _on_area_2d_mouse_entered() -> void:
+	pass # Replace with function body.
--- Scenes/Activities/Calandar/calandar.gd
+++ Scenes/Activities/Calandar/calandar.gd
@@ -1,16 +1,34 @@
-extends Node3D
+extends Node2D
+
+@onready var calendarPages : CPUParticles3D = $SubViewportContainer/SubViewport/CPUParticles3D
+var is_dragging = false
+var click_position = Vector2.ZERO
 
 func _ready():
 	pass
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		$SubViewportContainer/SubViewport/CPUParticles3D.emitting = true
+		calendarPages.emitting = true
+	if is_dragging:
+		global_position = get_global_mouse_position() - click_position
 
+func _on_input_event(viewport, event, shape_idx):
+	if event.is_action_pressed("Click"):
+		is_dragging = true
+		click_position = get_local_mouse_position()
+		print("Shape clicked")
+
+func _input(event):
+	if event.is_action_released("Click"):
+		is_dragging = false
+
+
+# Ending function, changes scenes
 func pass_days():
 	if GameManager.getAge() == [0] || GameManager.getAge() == [1] || GameManager.getAge() == [2]:
-		SceneManager.swap_scenes("res://Scenes/Activities/HappyBirthday/Birthday_ToddlerToPreTeen.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Activities/HappyBirthday/Birthday_ToddlerToPreTeen.tscn")
 	elif GameManager.getAge() == [3] || GameManager.getAge() == [4]:
-		SceneManager.swap_scenes("res://Scenes/Activities/HappyBirthday/Birthday_TeenAndAdult.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Activities/HappyBirthday/Birthday_TeenAndAdult.tscn")
 	else:
 		print("Not able to pass days, age not found.")
